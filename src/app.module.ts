909f7134f423a0b856d87ffaebbeb784ca69aba6
Thai Hua <huavietthai299@gmail.com>
2026-02-08 12:09:28 +0700

    feat(backend): setup Mongoose and generate Users, Customers, Pets resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CustomersModule } from './customers/customers.module';
+import { PetsModule } from './pets/pets.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
+    UsersModule,
+    CustomersModule,
+    PetsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
